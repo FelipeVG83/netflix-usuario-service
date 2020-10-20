0db238c52f131e2b2628e33be5fc6bbbd7f50661
Felipe Gonzalez <felipev.gonzalez@hotmail.com>
2020-10-20 00:35:37 -0300

    Adicionado data.sql

--- netflix-usuario-service/src/main/resources/data.sql
+++ netflix-usuario-service/src/main/resources/data.sql
@@ -1,10 +1,4 @@
-CREATE TABLE usuario (id int PRIMARY KEY, nome varchar(100));
-
-CREATE TABLE assistidos (idUsuario int, idFilme int);
-
-CREATE TABLE interesse (idUsuario int, idFilme int);
-
-
+--popular tabelas
 INSERT INTO USUARIO (id, nome) VALUES (1, 'Bruno Cavalcante');
 INSERT INTO USUARIO (id, nome) VALUES (2, 'Felipe Gonzalez');
 INSERT INTO USUARIO (id, nome) VALUES (3, 'Gabriel Muniz');
@@ -22,4 +16,3 @@ INSERT INTO INTERESSE (idUsuario, idFilme) VALUES (2, 1);
 INSERT INTO INTERESSE (idUsuario, idFilme) VALUES (2, 2);
 INSERT INTO INTERESSE (idUsuario, idFilme) VALUES (3, 1);
 INSERT INTO INTERESSE (idUsuario, idFilme) VALUES (3, 2);
-
