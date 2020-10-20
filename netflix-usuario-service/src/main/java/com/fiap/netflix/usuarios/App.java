68da9b9f62cff41255756cd013f8c8b3087217fa
Felipe Gonzalez <felipev.gonzalez@hotmail.com>
2020-10-20 00:36:27 -0300

    Implementado eureka e zuul

--- netflix-usuario-service/src/main/java/com/fiap/netflix/usuarios/App.java
+++ netflix-usuario-service/src/main/java/com/fiap/netflix/usuarios/App.java
@@ -2,13 +2,13 @@ package com.fiap.netflix.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class App {
 
 	public static void main(String[] args) {
